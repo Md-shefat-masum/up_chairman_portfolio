f5374c681d462127f4e4bc471de608f1288e7ba3
Md-shefat-masum <mshefat924@gmail.com>
2024-02-07 02:31:57 +0600

    note tasks

---
+++ suggestion.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * 1. inion porishod dropdown hover korle nambe.
+ * 2. union porishod o samajik kajer sub categories korte hobe.
+ * 3. union porishod o samajik kej er details page e all sub-categories link        dekhate hobe.
+ * 4. date banglai dekhate hobe.
+ * 5. post-details page e somproti poste view ta thakbena.
+ * 6. pagination make dynamic.
+ * 7. search dynamic.
+ * 8. home page ekta borho search option thakbe.
+ */
